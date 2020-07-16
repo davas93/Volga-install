c6414b98247e65c8b98ea33d1b070f24cec3d9d7
davas93 <vasin.da93@gmail.com>
2020-07-16 21:30:45 +0300

    Добавление выпадающего меню

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@
 $(".menu-button").on("click", function (e) {
   e.preventDefault;
   $(this).toggleClass("menu-button_active");
+  $(".header-menu").toggleClass("header-menu_active");
+});
+$(".header-menu__item").on("click", function () {
+  $(".header-menu").removeClass("header-menu_active");
+  $(".menu-button").removeClass("menu-button_active");
 });
