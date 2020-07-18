f87d7afc985fd1eb9c9d7f2f9d88543343396287
davas93 <vasin.da93@gmail.com>
2020-07-18 20:57:18 +0300

    Стилизация блока main

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Анимация кнопки menu button
+//Анимация кнопки menu button и выпадающего меню
 $(".menu-button").on("click", function (e) {
   e.preventDefault;
   $(this).toggleClass("menu-button_active");
@@ -7,4 +7,8 @@ $(".menu-button").on("click", function (e) {
 $(".header-menu__item").on("click", function () {
   $(".header-menu").removeClass("header-menu_active");
   $(".menu-button").removeClass("menu-button_active");
+});
+//маска ввода телефона
+$(function () {
+  $(".calc-form__input-phone").mask("+7(999) 999-9999");
 });
